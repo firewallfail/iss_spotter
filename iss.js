3a11e187d2466e3bee918a3a6be35d97cbc5bce3
firewallfail <firewall.fail@hotmail.com>
2022-04-14 17:19:20 -0400

    forgot to swap dummy values for actual variables

--- iss.js
+++ iss.js
@@ -55,9 +55,9 @@ const fetchISSFlyOverTimes = function(coords, callback) {
 const nextISSTimesForMyLocation = function(callback) {
   fetchMyIP((error, ip) => {
     if (error) return callback(error, null);
-    fetchCoordsByIP('38.80.107.155', (error, coords) => {
+    fetchCoordsByIP(ip, (error, coords) => {
       if (error) return callback(error, null);
-      fetchISSFlyOverTimes({ latitude: '49.27670', longitude: '-123.13000' }, (error, times) => {
+      fetchISSFlyOverTimes(coords, (error, times) => {
         if (error) return callback(error, null);
         callback(null, times);
       });
