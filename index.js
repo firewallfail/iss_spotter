588d397dff13b671cb61ff9f18e7ce2bc3b65db9
firewallfail <firewall.fail@hotmail.com>
2022-04-14 16:44:49 -0400

    main functions work on their own now

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
-const { fetchMyIP } = require('./iss');
+// const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
-fetchMyIP((error, ip) => {
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log("It worked! Returned IP:", ip)
+// })
+
+// fetchCoordsByIP('38.80.107.155', (error, coords) => {
+//   if (error) {
+//     console.log("It didn't work", error);
+//     return;
+//   }
+//   console.log(`Latitude: ${coords.latitude}\nLongitude: ${coords.longitude}`)
+// });
+//{ latitude: '49.27670', longitude: '-123.13000' }
+
+// fetchISSFlyOverTimes({ latitude: '49.27670', longitude: '-123.13000' }, (error, times) => {
+//   if (error) {
+//     console.log("It didn't work", error);
+//     return;
+//   }
+//   console.log(times);
+// });
+
+// index.js
+
+const { nextISSTimesForMyLocation } = require('./iss');
+
+nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
-    console.log("It didn't work!", error);
-    return;
+    return console.log("It didn't work!", error);
   }
-
-  console.log("It worked! Returned IP:", ip)
-})
+  // success, print out the deets!
+  console.log(passTimes);
+});
--- iss.js
+++ iss.js
@@ -10,9 +10,65 @@ const request = require('request');
 const fetchMyIP = function(callback) {
   // use request to fetch IP address from JSON API
   request('https://api.ipify.org?format=json', (error, response, body) => {
-    if (error) callback(error, null);
+    // inside the request callback ...
+    // error can be set if invalid domain, user is offline, etc.
+    if (error) return callback(error, null);
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    // if we get here, all's well and we got the data
     callback(null, JSON.parse(body).ip);
-  })
-}
+  });
+};
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) return callback(error, null);
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching Lat/Lon. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    callback(null, {
+      latitude: JSON.parse(body).latitude,
+      longitude: JSON.parse(body).longitude
+    });
+
+  });
+};
+
+const fetchISSFlyOverTimes = function(coords, callback) {
+  request(`https://iss-pass.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`, (error, response, body) => {
+    if (error) return callback(error, null);
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching ISS flyover data. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    callback(null, JSON.parse(body).response);
+  });
+};
+
+const nextISSTimesForMyLocation = function(callback) {
+
+};
+
+module.exports = {
+  fetchMyIP,
+  fetchCoordsByIP,
+  fetchISSFlyOverTimes,
+  nextISSTimesForMyLocation
+};
+
+
+//My original solution
+// const fetchMyIP = function(callback) {
+//   // use request to fetch IP address from JSON API
+//   request('https://api.ipify.org?format=json', (error, response, body) => {
+//     if (error) callback(error, null);
+//     callback(null, JSON.parse(body).ip);
+//   })
+// }
